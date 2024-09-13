6cf61609f246a036f1e75f3a837fa7f8715fa03b
Phoomss <narongsak5az@gmail.com>
2024-09-13 18:54:08 +0700

    config tang

--- spring-api/src/main/kotlin/com/example/spring_api/config/OpenApiConfig.kt
+++ spring-api/src/main/kotlin/com/example/spring_api/config/OpenApiConfig.kt
@@ -9,11 +9,20 @@ import org.springframework.context.annotation.Configuration
 class OpenApiConfig {
     @Bean
     fun customOpenAPI(): OpenAPI {
-        return OpenAPI().info(
-            Info()
-                .title("Store API with Spring Boot and PostgreSQL")
-                .version("1.0")
-                .description("This is Store API using Spring Boot and PostgreSQL")
-        )
+        return OpenAPI()
+            .info(
+                Info()
+                    .title("Store API with Spring Boot and PostgreSQL")
+                    .version("1.0")
+                    .description("This is Store API using Spring Boot and PostgreSQL")
+            )
+
+//            custom tags api
+            .tags(
+                listOf(
+                    io.swagger.v3.oas.models.tags.Tag().name("Categories").description("This APIs from managing categories"),
+                    io.swagger.v3.oas.models.tags.Tag().name("Products").description("This APIs from managing products")
+                )
+            )
     }
 }
