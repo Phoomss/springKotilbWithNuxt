7939f7c06025612564e85ca5b66d62d1cf57413f
Phoomss <narongsak5az@gmail.com>
2024-09-13 18:53:36 +0700

    add tag swagger

--- spring-api/src/main/kotlin/com/example/spring_api/controllers/CategoryController.kt
+++ spring-api/src/main/kotlin/com/example/spring_api/controllers/CategoryController.kt
@@ -2,6 +2,8 @@ package com.example.spring_api.controllers
 
 import com.example.spring_api.models.Category
 import com.example.spring_api.services.CategoryService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -14,15 +16,18 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
+@Tag(name = "Categories", description = "APIs for managing categories")
 @RestController
 @RequestMapping("/api/v1/categories")
 class CategoryController(private val categoryService: CategoryService) {
+    @Operation(summary = "Get all categories", description = "Get all categories")
     @GetMapping
     fun getCategories(): ResponseEntity<List<Category>> {
         val categories = categoryService.getAllCategories()
         return ResponseEntity.ok(categories)
     }
 
+    @Operation(summary = "Get categories by ID", description = "Get categories by ID from database")
     @GetMapping("/{id}")
     fun getCategory(@PathVariable id: Int): ResponseEntity<Category> {
         val category = categoryService.getCategoryById(id)
@@ -33,12 +38,14 @@ class CategoryController(private val categoryService: CategoryService) {
         }
     }
 
+    @Operation(summary = "Add new category", description = "Add new category to database")
     @PostMapping
     fun addCategory(@RequestBody category: Category): ResponseEntity<Category> {
         val savedCategory = categoryService.addCategory(category)
         return ResponseEntity.ok(savedCategory)
     }
 
+    @Operation(summary = "Update category by ID", description = "Update category by ID from database")
     @PutMapping("/{id}")
     fun updateCategory(@PathVariable id: Int, @RequestBody category: Category): ResponseEntity<Category> {
         val updatedCategory = categoryService.updateCategory(id, category)
@@ -49,6 +56,7 @@ class CategoryController(private val categoryService: CategoryService) {
         }
     }
 
+    @Operation(summary = "Delete category by ID", description = "Delete category by ID from database")
     @DeleteMapping("/{id}")
     fun deleteCategory(@PathVariable id: Int): ResponseEntity<Category> {
         val deletedCategory = categoryService.deleteCategory(id)
--- spring-api/src/main/kotlin/com/example/spring_api/controllers/ProductController.kt
+++ spring-api/src/main/kotlin/com/example/spring_api/controllers/ProductController.kt
@@ -2,6 +2,8 @@ package com.example.spring_api.controllers
 
 import com.example.spring_api.models.Product
 import com.example.spring_api.services.ProductService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
@@ -14,12 +16,15 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "Products", description = "APIs for managing products")
 @RestController
 @RequestMapping("/api/products")
 class ProductController(private val productService: ProductService) {
+    @Operation(summary = "Get all products", description = "Get all products from database ")
     @GetMapping
     fun getAllProducts() = productService.getAllProducts()
 
+    @Operation(summary = "Get product by ID", description = "Get product by ID from database")
     @GetMapping("/{id}")
     fun getProductById(@PathVariable id: Int): ResponseEntity<Product> {
         val product = productService.getProductById(id)
@@ -27,18 +32,21 @@ class ProductController(private val productService: ProductService) {
             .orElseGet { ResponseEntity(HttpStatus.NOT_FOUND) }
     }
 
+    @Operation(summary = "Add new Product", description = "Post product from database")
     @PostMapping("/create")
     fun createProduct(@RequestBody product: Product): ResponseEntity<Product> {
         val createProduct = productService.createProduct(product)
         return ResponseEntity(createProduct, HttpStatus.CREATED)
     }
 
+    @Operation(summary = "Update product by ID", description = "Update product by ID from database")
     @PutMapping("/{id}")
     fun updateProduct(@PathVariable id: Int, @RequestBody product: Product): ResponseEntity<Product> {
         val updateProduct = productService.updateProduct(id, product)
         return ResponseEntity.ok(updateProduct)
     }
 
+    @Operation(summary = "Delete product by ID", description = "Delete product by ID from database")
     @DeleteMapping("/{id}")
     fun deleteProduct(@PathVariable id: Int): ResponseEntity<Void> {
         productService.deleteProdut(id)
