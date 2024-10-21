1026a6702caa0fe83fcf0cd7aa3d723a612f7f6c
phoomss <narongsak5az@gmail.com>
2024-10-21 18:55:53 +0700

    success backend dashboard

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,13 +1,9 @@
-<script lang="ts" setup></script>
-
 <template>
-  <div>
-    <VApp>
-      <VMain>
-        <slot />
-      </VMain>
-    </VApp>
-  </div>
+	<div>
+		<VApp>
+			<VMain>
+				<slot />
+			</VMain>
+		</VApp>
+	</div>
 </template>
-
-<style></style>
